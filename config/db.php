b7285ee2db27125f8efe216df4d7c45cedae0495
Semmy Purewal <semmy@semmy.me>
2018-04-16 23:42:55 -0500

    modified the first file

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = "";
-$user = "";
-$password = "";
-$dbname = "";
-$port = "";
+$host = "ec2-107-21-126-193.compute-1.amazonaws.com";
+$user = "qqhzabyknffnnr";
+$password = "5112815ae02d241017fb545bd6f30f1db410eb69a996e34a541518ce276f0d77";
+$dbname = "d4mo7cj6hcv03i";
+$port = "5432";
 
 try{
   //Set DSN data source name
